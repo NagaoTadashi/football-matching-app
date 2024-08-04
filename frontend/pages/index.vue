12cc6f94787d4fe3405f744d1aa49690e552ea13
tadashi <tahofu89@gmail.com>
2024-08-04 17:50:32 +0900

    試合日程・結果ページの雛形を作成しました

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,3 +1,10 @@
+<script setup>
+import Matches from '~/components/matches.vue';
+</script>
+
 <template>
-    <div></div>
+    <div>
+        <h1>試合日程・結果</h1>
+        <Matches></Matches>
+    </div>
 </template>
