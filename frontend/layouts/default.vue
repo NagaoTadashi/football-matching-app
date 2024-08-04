f0122cfb27700ee89be5e440fabf0a166181838e
tadashi <tahofu89@gmail.com>
2024-08-04 18:33:47 +0900

    ナビゲーションのドロワー部分を非コンポーネント化しました

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,8 +1,31 @@
 <script setup>
-import { ref } from 'vue';
-import NavigationDrawer from '~/components/NavigationDrawer.vue';
+import { ref, watch } from 'vue';
 
+const items = [
+    {
+        title: 'ホーム',
+        value: 'home',
+    },
+    {
+        title: '試合日程・結果',
+        value: 'match',
+    },
+    {
+        title: 'メンバー',
+        value: 'mumber',
+    },
+    {
+        title: '対戦相手を探す',
+        value: 'findOpponent',
+    },
+];
+
+const group = ref(null);
 const drawer = ref(null);
+
+watch(group, () => {
+    drawer.value = false;
+});
 </script>
 
 <template>
@@ -20,10 +43,16 @@ const drawer = ref(null);
         <v-app-bar>
             <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-            <v-app-bar-title>Application</v-app-bar-title>
+            <v-app-bar-title>Football Macth</v-app-bar-title>
         </v-app-bar>
 
-        <NavigationDrawer v-model="drawer" />
+        <v-navigation-drawer
+            v-model="drawer"
+            :location="$vuetify.display.mobile ? 'bottom' : undefined"
+            temporary
+        >
+            <v-list :items="items"></v-list>
+        </v-navigation-drawer>
 
         <v-main class="bg-grey-lighten-2">
             <v-container>
