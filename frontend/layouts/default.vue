0fe003b2945a4f53cc6861fea7e5e81a0936ed83
tadashi <tahofu89@gmail.com>
2024-08-04 17:51:38 +0900

    v-mainの中身をslot化しました

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -27,17 +27,7 @@ const drawer = ref(null);
 
         <v-main class="bg-grey-lighten-2">
             <v-container>
-                <v-row>
-                    <template v-for="n in 4" :key="n">
-                        <v-col class="mt-2" cols="12">
-                            <strong>Category {{ n }}</strong>
-                        </v-col>
-
-                        <v-col v-for="j in 6" :key="`${n}${j}`" cols="6" md="2">
-                            <v-sheet height="150"></v-sheet>
-                        </v-col>
-                    </template>
-                </v-row>
+                <slot />
             </v-container>
         </v-main>
     </v-app>
