27b5017e661dd3263b8e51adc042d0e65a3dd1a7
tadashi <tahofu89@gmail.com>
2024-08-13 15:41:30 +0900

    データの中身を変更しました

--- frontend/pages/MemberList.vue
+++ frontend/pages/MemberList.vue
@@ -1,149 +1,164 @@
 <script setup>
 import { ref } from 'vue';
+import PlayerCard from '~/components/pages/PlayerCard.vue';
 
 const member = {
     player1: {
         id: 1,
-        number: 1,
-        name: 'player1',
         position: 'GK',
+        number: 1,
+        namae: '長尾 考梓',
+        name: 'Tadashi Nagao',
     },
     player2: {
         id: 2,
-        number: 2,
-        name: 'player2',
         position: 'GK',
+        number: 2,
+        namae: '長尾 考梓',
+        name: 'Tadashi Nagao',
     },
     player3: {
         id: 3,
-        number: 3,
-        name: 'player3',
         position: 'DF',
+        number: 3,
+        namae: '長尾 考梓',
+        name: 'Tadashi Nagao',
     },
     player4: {
         id: 4,
-        number: 4,
-        name: 'player4',
         position: 'DF',
+        number: 4,
+        namae: '長尾 考梓',
+        name: 'Tadashi Nagao',
     },
     player5: {
         id: 5,
-        number: 5,
-        name: 'player5',
         position: 'DF',
+        number: 5,
+        namae: '長尾 考梓',
+        name: 'Tadashi Nagao',
     },
     player6: {
         id: 6,
-        number: 6,
-        name: 'player6',
         position: 'MF',
+        number: 6,
+        namae: '長尾 考梓',
+        name: 'Tadashi Nagao',
     },
     player7: {
         id: 7,
-        number: 7,
-        name: 'player7',
         position: 'MF',
+        number: 7,
+        namae: '長尾 考梓',
+        name: 'Tadashi Nagao',
     },
     player8: {
         id: 8,
-        number: 8,
-        name: 'player8',
         position: 'MF',
+        number: 8,
+        namae: '長尾 考梓',
+        name: 'Tadashi Nagao',
     },
     player9: {
         id: 9,
-        number: 9,
-        name: 'player9',
         position: 'MF',
+        number: 9,
+        namae: '長尾 考梓',
+        name: 'Tadashi Nagao',
     },
     player10: {
         id: 10,
-        number: 10,
-        name: 'player10',
         position: 'MF',
+        number: 10,
+        namae: '長尾 考梓',
+        name: 'Tadashi Nagao',
     },
     player11: {
         id: 11,
-        number: 11,
-        name: 'player11',
         position: 'FW',
+        number: 11,
+        namae: '長尾 考梓',
+        name: 'Tadashi Nagao',
     },
     player12: {
         id: 12,
-        number: 12,
-        name: 'player12',
         position: 'FW',
+        number: 12,
+        namae: '長尾 考梓',
+        name: 'Tadashi Nagao',
     },
     player13: {
         id: 13,
-        number: 13,
-        name: 'player13',
         position: 'FW',
+        number: 13,
+        namae: '長尾 考梓',
+        name: 'Tadashi Nagao',
     },
     player14: {
         id: 14,
-        number: 14,
-        name: 'player14',
         position: 'FW',
+        number: 14,
+        namae: '長尾 考梓',
+        name: 'Tadashi Nagao',
     },
     player15: {
         id: 15,
-        number: 15,
-        name: 'player15',
         position: 'FW',
+        number: 15,
+        namae: '長尾 考梓',
+        name: 'Tadashi Nagao',
     },
     player16: {
         id: 16,
-        number: 16,
-        name: 'player16',
         position: 'FW',
+        number: 16,
+        namae: '長尾 考梓',
+        name: 'Tadashi Nagao',
     },
     player17: {
         id: 17,
-        number: 17,
-        name: 'player17',
         position: 'FW',
+        number: 17,
+        namae: '長尾 考梓',
+        name: 'Tadashi Nagao',
     },
     player18: {
         id: 18,
-        number: 18,
-        name: 'player18',
         position: 'FW',
+        number: 18,
+        namae: '長尾 考梓',
+        name: 'Tadashi Nagao',
     },
     player19: {
         id: 19,
-        number: 19,
-        name: 'player19',
         position: 'FW',
+        number: 19,
+        namae: '長尾 考梓',
+        name: 'Tadashi Nagao',
     },
     player20: {
         id: 20,
-        number: 20,
-        name: 'player20',
         position: 'FW',
+        number: 20,
+        namae: '長尾 考梓',
+        name: 'Tadashi Nagao',
     },
 };
-
-const color = ref('primary');
-const variant = ref('tonal');
 </script>
 
 <template>
     <div>
         <v-row>
-            <v-col class="mt-2" cols="12">
+            <v-col cols="6">
                 <h1>メンバー一覧</h1>
             </v-col>
+            <v-col cols="6" class="d-flex justify-end">
+                <v-btn>メンバーを追加</v-btn>
+            </v-col>
+        </v-row>
+        <v-row>
             <v-col v-for="(player, i) in member" :key="i" cols="12" md="4">
-                <v-card :variant="variant" class="mx-auto" elevation="3">
-                    <v-card-item>
-                        <v-card-title>No. {{ player.number }} </v-card-title>
-                        <v-card-title
-                            >{{ player.position }}
-                            {{ player.name }}</v-card-title
-                        >
-                    </v-card-item>
-                </v-card>
+                <PlayerCard :player="player" />
             </v-col>
         </v-row>
     </div>
