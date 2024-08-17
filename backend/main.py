eb15e22796437a3ea025b1a6df83b5d5b71e1d39
tadashi <tahofu89@gmail.com>
2024-08-17 20:45:05 +0900

    メンバー一覧をバックエンドから取得する実装に変更しました

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
@@ -12,6 +13,18 @@ class Player(BaseModel):
     namae: str
     name: str
 
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 member = [
     Player(id=1, position="GK", namae="長尾 考梓", name="Tadashi Nagao", number=1),
