8ea2f40b531b0ad3a22fce2f1d281f5ee2eada0c
tadashi <tahofu89@gmail.com>
2024-08-17 22:06:55 +0900

    試合カードをコンポーネント化しました

--- frontend/pages/MatchList.vue
+++ frontend/pages/MatchList.vue
@@ -1,8 +1,5 @@
 <script setup>
-import { ref } from 'vue';
-
-const color = ref('primary');
-const variant = ref('tonal');
+import MatchCard from '~/components/pages/matchlist/MatchCard.vue';
 
 const { data: matches } = await useFetch('http://localhost:8000/matches');
 </script>
@@ -15,29 +12,7 @@ const { data: matches } = await useFetch('http://localhost:8000/matches');
         </v-row>
         <v-row>
             <v-col v-for="(match, i) in matches" :key="i" cols="12" md="4">
-                <v-card
-                    :variant="variant"
-                    class="mx-auto"
-                    elevation="3"
-                    hover
-                    link
-                    :to="{ name: 'matches-id', params: { id: match.id } }"
-                >
-                    <v-card-item>
-                        <v-card-title>vs {{ match.opponent }}</v-card-title>
-                        <v-card-subtitle
-                            >日時: {{ match.date }}
-                            {{ match.time }}</v-card-subtitle
-                        >
-                        <v-card-subtitle
-                            >場所: {{ match.venue }}</v-card-subtitle
-                        >
-                        <v-card-subtitle
-                            >結果: {{ match.my_team_score }} -
-                            {{ match.opponent_score }}</v-card-subtitle
-                        >
-                    </v-card-item>
-                </v-card>
+                <MatchCard :match="match" />
             </v-col>
         </v-row>
     </div>
