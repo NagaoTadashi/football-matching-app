782a7ef8196459b22dba5d2c403c035f55ea8cc9
tadashi <tahofu89@gmail.com>
2024-08-23 21:05:02 +0900

    メンバー情報を取得・登録する実装を追加しました

--- backend/crud.py
+++ backend/crud.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from . import models, schemas
 
 
+# Match
 def get_matches(db: Session):
     return db.query(models.Match).all()
 
@@ -20,3 +21,21 @@ def create_match(db: Session, match: schemas.MatchCreate):
     db.commit()
     db.refresh(db_match)
     return db_match
+
+
+# Player
+def get_players(db: Session):
+    return db.query(models.Player).all()
+
+
+def create_player(db: Session, player: schemas.PlayerCreate):
+    db_player = models.Player(
+        position=player.position,
+        namae=player.namae,
+        name=player.name,
+        number=player.number,
+    )
+    db.add(db_player)
+    db.commit()
+    db.refresh(db_player)
+    return db_player
