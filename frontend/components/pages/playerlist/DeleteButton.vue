9c650e0c958558292ddb102f002423463d3905bf
tadashi <tahofu89@gmail.com>
2024-08-25 14:00:52 +0900

    消去ボタンコンポーネントを追加しました

---
+++ frontend/components/pages/playerlist/DeleteButton.vue
@@ -0,0 +1,54 @@
+<script setup>
+import { ref } from 'vue';
+
+const dialog = ref(false);
+
+const props = defineProps(['player']);
+
+const emit = defineEmits(['PlayerDeleted']);
+
+async function deletePlayer() {
+    const deletedPlayer = await $fetch(
+        `http://localhost:8000/player/${props.player.id}`,
+        {
+            method: 'DELETE',
+        }
+    );
+    if (deletedPlayer) {
+        // ダイアログを閉じる
+        dialog.value = false;
+        // イベントを発火して親コンポーネントにプレイヤー情報を渡す
+        emit('PlayerDeleted', deletedPlayer);
+    }
+}
+</script>
+
+<template>
+    <div class="text-center pa-4">
+        <v-btn
+            @click="dialog = true"
+            prepend-icon="mdi-delete"
+            text="削除"
+            elevation="5"
+        ></v-btn>
+
+        <v-dialog v-model="dialog" width="auto">
+            <v-card max-width="400" title="この選手情報を削除しますか？">
+                <template v-slot:actions>
+                    <v-spacer></v-spacer>
+                    <v-btn
+                        text="キャンセル"
+                        variant="plain"
+                        @click="dialog = false"
+                    ></v-btn>
+                    <v-btn
+                        color="primary"
+                        text="Ok"
+                        variant="tonal"
+                        @click="deletePlayer"
+                    ></v-btn>
+                </template>
+            </v-card>
+        </v-dialog>
+    </div>
+</template>
