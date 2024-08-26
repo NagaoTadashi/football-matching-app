6d5fe8b68dbdc56ec8e30fb2448f3f89e806d752
tadashi <tahofu89@gmail.com>
2024-08-26 21:47:26 +0900

    タイトルを消去しました

--- frontend/pages/MatchList.vue
+++ frontend/pages/MatchList.vue
@@ -3,13 +3,9 @@ import MatchCard from '~/components/pages/matchlist/MatchCard.vue';
 
 const { data: matches } = await useFetch('http://localhost:8000/matches');
 </script>
+
 <template>
     <div>
-        <v-row>
-            <v-col cols="6">
-                <h1>試合日程・結果</h1>
-            </v-col>
-        </v-row>
         <v-row>
             <v-col v-for="(match, i) in matches" :key="i" cols="12" md="4">
                 <MatchCard :match="match" />
