cc0df52aa4bed21ad8300630873da4aa413a8374
tadashi <tahofu89@gmail.com>
2024-08-26 22:52:10 +0900

    index.vueにMatchList.vueの内容をコピーしました

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,1 +1,15 @@
-<template></template>
+<script setup>
+import MatchCard from '~/components/pages/matchlist/MatchCard.vue';
+
+const { data: matches } = await useFetch('http://localhost:8000/matches');
+</script>
+
+<template>
+    <div>
+        <v-row>
+            <v-col v-for="(match, i) in matches" :key="i" cols="12" md="4">
+                <MatchCard :match="match" />
+            </v-col>
+        </v-row>
+    </div>
+</template>
