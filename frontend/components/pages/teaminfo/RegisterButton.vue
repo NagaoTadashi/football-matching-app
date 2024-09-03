1ff8e4a29bd909e1461b07331ae2fe376c789508
tadashi <tahofu89@gmail.com>
2024-09-03 22:49:54 +0900

    フォームの内容を修正しました

--- frontend/components/pages/teaminfo/RegisterButton.vue
+++ frontend/components/pages/teaminfo/RegisterButton.vue
@@ -2,65 +2,73 @@
 import { reactive, shallowRef } from 'vue';
 
 const dialog = shallowRef(false);
-const region = [
-    '北海道',
-    '青森県',
-    '岩手県',
-    '宮城県',
-    '秋田県',
-    '山形県',
-    '福島県',
-    '茨城県',
-    '栃木県',
-    '群馬県',
-    '埼玉県',
-    '千葉県',
-    '東京都',
-    '神奈川県',
-    '新潟県',
-    '富山県',
-    '石川県',
-    '福井県',
-    '山梨県',
-    '長野県',
-    '岐阜県',
-    '静岡県',
-    '愛知県',
-    '三重県',
-    '滋賀県',
-    '京都府',
-    '大阪府',
-    '兵庫県',
-    '奈良県',
-    '和歌山県',
-    '鳥取県',
-    '島根県',
-    '岡山県',
-    '広島県',
-    '山口県',
-    '徳島県',
-    '香川県',
-    '愛媛県',
-    '高知県',
-    '福岡県',
-    '佐賀県',
-    '長崎県',
-    '熊本県',
-    '大分県',
-    '宮崎県',
-    '鹿児島県',
-    '沖縄県',
-];
-const category = ['社会人', '大学', '高校', '中学', '小学'];
-
-const emit = defineEmits(['TeamInfoRegisterd']);
 
 const teamInfo = reactive({
     name: '',
     region: '',
+    prefecture: '',
     category: '',
     league: '',
 });
+
+const regions = {
+    '北海道・東北': [
+        '北海道',
+        '青森県',
+        '岩手県',
+        '宮城県',
+        '秋田県',
+        '山形県',
+        '福島県',
+    ],
+    関東: [
+        '茨城県',
+        '栃木県',
+        '群馬県',
+        '埼玉県',
+        '千葉県',
+        '東京都',
+        '神奈川県',
+    ],
+    中部: [
+        '新潟県',
+        '富山県',
+        '石川県',
+        '福井県',
+        '山梨県',
+        '長野県',
+        '岐阜県',
+        '静岡県',
+        '愛知県',
+    ],
+    近畿: [
+        '三重県',
+        '滋賀県',
+        '京都府',
+        '大阪府',
+        '兵庫県',
+        '奈良県',
+        '和歌山県',
+    ],
+    中国: ['鳥取県', '島根県', '岡山県', '広島県', '山口県'],
+    四国: ['徳島県', '香川県', '愛媛県', '高知県'],
+    '九州・沖縄': [
+        '福岡県',
+        '佐賀県',
+        '長崎県',
+        '熊本県',
+        '大分県',
+        '宮崎県',
+        '鹿児島県',
+        '沖縄県',
+    ],
+};
+
+const regionsList = Object.keys(regions);
+
+const category = ['社会人', '大学', '高校', '中学', '小学'];
+
+const emit = defineEmits(['TeamInfoRegisterd']);
 
 async function registerTeamInfo() {
     const newTeamInfo = await $fetch('http://localhost:8000/team_info', {
@@ -74,6 +82,7 @@ async function registerTeamInfo() {
     // フォームデータをクリアする
     teamInfo.name = '';
     teamInfo.region = '';
+    teamInfo.prefecture = '';
     teamInfo.category = '';
     teamInfo.league = '';
 }
@@ -97,32 +106,39 @@ async function registerTeamInfo() {
                     <v-row dense>
                         <v-col cols="12" md="6" sm="6">
                             <v-text-field
-                                label="チーム名"
                                 v-model="teamInfo.name"
+                                label="チーム名を入力"
                             ></v-text-field>
                         </v-col>
                     </v-row>
                     <v-row>
-                        <v-col cols="12" md="3" sm="6">
+                        <v-col cols="12" md="4" sm="6">
                             <v-select
-                                :items="region"
-                                density="default"
-                                label="地域"
                                 v-model="teamInfo.region"
+                                :items="regionsList"
+                                label="地域を選択"
                             ></v-select>
                         </v-col>
                         <v-col cols="12" md="4" sm="6">
                             <v-select
-                                :items="category"
-                                density="default"
-                                label="カテゴリ"
+                                v-model="teamInfo.prefecture"
+                                :items="regions[teamInfo.region]"
+                                label="都道府県を選択"
+                            ></v-select>
+                        </v-col>
+                    </v-row>
+                    <v-row>
+                        <v-col cols="12" md="4" sm="6">
+                            <v-select
                                 v-model="teamInfo.category"
+                                :items="category"
+                                label="カテゴリを選択"
                             ></v-select>
                         </v-col>
                         <v-col cols="12" md="8" sm="6">
                             <v-text-field
-                                label="所属リーグ"
                                 v-model="teamInfo.league"
+                                label="所属リーグを入力"
                             ></v-text-field>
                         </v-col>
                     </v-row>
