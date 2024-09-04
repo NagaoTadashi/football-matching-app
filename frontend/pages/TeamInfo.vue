d70d40bdbb6028fac2633ae2a69ee6dd89d18ca0
tadashi <tahofu89@gmail.com>
2024-09-04 21:05:16 +0900

    チーム情報ページに編集ボタンを追加しました

--- frontend/pages/TeamInfo.vue
+++ frontend/pages/TeamInfo.vue
@@ -1,13 +1,82 @@
 <script setup>
-const { data: team_info } = await useFetch('http://localhost:8000/team_info');
+import RegisterButton from '~/components/pages/teaminfo/RegisterButton.vue';
+import EditButton from '~/components/pages/teaminfo/EditButton.vue';
+
+const { data: teamInfo } = await useFetch('http://localhost:8000/team_info');
+
+function handleTeamInfoRegisterd(newTeamInfo) {
+    teamInfo.value = newTeamInfo;
+}
+
+function handleTeamInfoEdited(updatedTeamInfo) {
+    teamInfo.value = updatedTeamInfo;
+}
 </script>
 
 <template>
-    <div v-if="team_info === null">
-        <v-empty-state
-            icon="mdi-tshirt-crew"
-            title="チーム情報が登録されていません"
+    <div>
+        <div
+            v-if="teamInfo === null"
+            class="d-flex align-center justify-center"
+            style="min-height: 300px"
         >
-        </v-empty-state>
+            <v-empty-state
+                icon="mdi-tshirt-crew"
+                title="チーム情報が登録されていません"
+            >
+            </v-empty-state>
+            <RegisterButton @TeamInfoRegisterd="handleTeamInfoRegisterd" />
+        </div>
+        <div v-else>
+            <v-row class="d-flex align-end justify-end">
+                <EditButton
+                    :teamInfo="teamInfo"
+                    @TeamInfoEdited="handleTeamInfoEdited"
+                />
+            </v-row>
+
+            <v-row justify="center">
+                <v-col cols="12" sm="20" md="20">
+                    <v-card>
+                        <v-row no-gutters>
+                            <v-col cols="12" sm="6">
+                                <v-card-title class="text-h4">{{
+                                    teamInfo.name
+                                }}</v-card-title>
+                                <br />
+                                <v-card-subtitle class="text-h6"
+                                    >地域:
+                                    {{ teamInfo.region }}</v-card-subtitle
+                                >
+                                <br />
+
+                                <v-card-subtitle class="text-h6"
+                                    >都道府県:
+                                    {{ teamInfo.prefecture }}</v-card-subtitle
+                                >
+                                <br />
+
+                                <v-card-subtitle class="text-h6"
+                                    >カテゴリ:
+                                    {{ teamInfo.category }}</v-card-subtitle
+                                >
+                                <br />
+
+                                <v-card-subtitle class="text-h6"
+                                    >所属リーグ:
+                                    {{ teamInfo.league }}</v-card-subtitle
+                                >
+                            </v-col>
+                            <v-col cols="12" sm="6">
+                                <v-img
+                                    src="https://cdn.pixabay.com/photo/2015/07/02/00/08/football-828218_1280.jpg"
+                                    class="fill-height"
+                                ></v-img>
+                            </v-col>
+                        </v-row>
+                    </v-card>
+                </v-col>
+            </v-row>
+        </div>
     </div>
 </template>
