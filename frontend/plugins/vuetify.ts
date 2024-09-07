f19b5e4cb29dc6779fe23ded2e6f6473007a5f48
tadashi <tahofu89@gmail.com>
2024-09-07 16:47:07 +0900

    プラグインを追加しました

--- frontend/plugins/vuetify.ts
+++ frontend/plugins/vuetify.ts
@@ -1,12 +1,19 @@
 // import this after install `@mdi/font` package
 import '@mdi/font/css/materialdesignicons.css';
-
 import 'vuetify/styles';
 import { createVuetify } from 'vuetify';
+import { VDateInput } from 'vuetify/labs/VDateInput';
+import { VNumberInput } from 'vuetify/labs/VNumberInput';
+import { VTimePicker } from 'vuetify/labs/VTimePicker';
 
 export default defineNuxtPlugin((app) => {
     const vuetify = createVuetify({
         // ... your configuration
+        components: {
+            VDateInput,
+            VNumberInput,
+            VTimePicker,
+        },
     });
     app.vueApp.use(vuetify);
 });
