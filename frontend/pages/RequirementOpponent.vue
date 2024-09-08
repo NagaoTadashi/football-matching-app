bc0c3f236930bc39d691822d868f7344569100e9
tadashi <tahofu89@gmail.com>
2024-09-08 16:43:06 +0900

    対戦相手を募集するページを追加しました

--- frontend/pages/RequirementOpponent.vue
+++ frontend/pages/RequirementOpponent.vue
@@ -1,1 +1,43 @@
-<template></template>
+<script setup>
+import RecruitmentCard from '~/components/pages/recruitmentopponent/RecruitmentCard.vue';
+import PostButton from '~/components/pages/recruitmentopponent/PostButton.vue';
+
+const { data: recruitments } = await useFetch(
+    'http://localhost:8000/recruitments'
+);
+
+function handleRecruitmentPosted(newRecruitment) {
+    recruitments.value.push(newRecruitment);
+}
+</script>
+
+<template>
+    <div>
+        <v-row class="d-flex align-end justify-end"
+            ><PostButton @recruitmentPosted="handleRecruitmentPosted"
+        /></v-row>
+        <div
+            v-if="recruitments.length === 0"
+            class="d-flex align-center justify-center"
+            style="min-height: 300px"
+        >
+            <v-empty-state
+                icon="mdi-text-box-outline"
+                title="募集が投稿されていません"
+            >
+            </v-empty-state>
+        </div>
+        <div v-else>
+            <v-row>
+                <v-col
+                    v-for="(recruitment, i) in recruitments"
+                    :key="i"
+                    cols="12"
+                    md="4"
+                >
+                    <RecruitmentCard :recruitment="recruitment" />
+                </v-col>
+            </v-row>
+        </div>
+    </div>
+</template>
