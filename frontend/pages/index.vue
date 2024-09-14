d2c74267dc0da5e6556fbfb12f4fc8b9fce1e125
tadashi <tahofu89@gmail.com>
2024-09-14 16:55:02 +0900

    divタグを追加しました

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -5,22 +5,24 @@ const { data: matches } = await useFetch('http://localhost:8000/matches');
 </script>
 
 <template>
-    <div
-        v-if="matches.length === 0"
-        class="d-flex align-center justify-center"
-        style="min-height: 300px"
-    >
-        <v-empty-state
-            icon="mdi-soccer-field"
-            title="試合日程・結果はまだありません"
+    <div>
+        <div
+            v-if="matches.length === 0"
+            class="d-flex align-center justify-center"
+            style="min-height: 300px"
         >
-        </v-empty-state>
-    </div>
-    <div v-else>
-        <v-row>
-            <v-col v-for="(match, i) in matches" :key="i" cols="12" md="4">
-                <MatchCard :match="match" />
-            </v-col>
-        </v-row>
+            <v-empty-state
+                icon="mdi-soccer-field"
+                title="試合日程・結果はまだありません"
+            >
+            </v-empty-state>
+        </div>
+        <div v-else>
+            <v-row>
+                <v-col v-for="(match, i) in matches" :key="i" cols="12" md="4">
+                    <MatchCard :match="match" />
+                </v-col>
+            </v-row>
+        </div>
     </div>
 </template>
