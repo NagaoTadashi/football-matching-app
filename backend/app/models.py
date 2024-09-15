51f186f718b55c237b49e65d24b60c6a9c1c968b
tadashi <tahofu89@gmail.com>
2024-09-15 16:55:33 +0900

    型をEnumに変更しました

--- backend/app/models.py
+++ backend/app/models.py
@@ -40,7 +40,7 @@ class Player(Base):
     __tablename__ = "players"
 
     id = Column(Integer, primary_key=True)
-    position = Column(String)
+    position = Column(Enum("GK", "DF", "MF", "FW"))
     number = Column(Integer)
     namae = Column(String)
     name = Column(String)
