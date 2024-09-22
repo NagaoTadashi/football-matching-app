980131f6ad86525a96125ef2885f17323c09ffd1
tadashi <tahofu89@gmail.com>
2024-09-22 17:28:15 +0900

    loginページを追加しました

---
+++ frontend/pages/login.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { onMounted } from 'vue';
+
+import firebase from 'firebase/compat/app';
+
+const { $ui } = useNuxtApp();
+
+onMounted(() => {
+    $ui.start('#firebaseui-auth-container', {
+        signInFlow: 'popup',
+        signInOptions: [
+            firebase.auth.EmailAuthProvider.PROVIDER_ID,
+            // firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        ],
+    });
+});
+
+definePageMeta({
+    layout: 'login',
+});
+</script>
+
+<template>
+    <div id="firebaseui-auth-container"></div>
+</template>
