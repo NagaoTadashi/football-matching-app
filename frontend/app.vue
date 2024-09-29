9843b3b1f02733865f2160e5c021872e141df563
tadashi <tahofu89@gmail.com>
2024-09-29 13:35:08 +0900

    ログイン状況を確認する処理を追加しました

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,3 +1,22 @@
+<script setup>
+const router = useRouter();
+const route = useRoute();
+const user = useCurrentUser();
+
+// we don't need this watcher on server
+onMounted(() => {
+    watch(user, (user, prevUser) => {
+        if (prevUser && !user) {
+            // user logged out
+            router.push('/login');
+        } else if (user && typeof route.query.redirect === 'string') {
+            // user logged in
+            router.push(route.query.redirect);
+        }
+    });
+});
+</script>
+
 <template>
     <NuxtLayout>
         <NuxtPage />
