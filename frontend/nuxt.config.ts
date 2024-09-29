99814943274450c751f50600d15508c6baa445f9
tadashi <tahofu89@gmail.com>
2024-09-29 13:38:52 +0900

    firebaseの認証を追加しました

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -3,6 +3,7 @@ import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify';
 export default defineNuxtConfig({
     compatibilityDate: '2024-04-03',
     devtools: { enabled: true },
+    ssr: false,
     build: {
         transpile: ['vuetify'],
     },
@@ -13,12 +14,27 @@ export default defineNuxtConfig({
                 config.plugins.push(vuetify({ autoImport: true }));
             });
         },
+        'nuxt-vuefire',
     ],
     vite: {
         vue: {
             template: {
                 transformAssetUrls,
             },
+        },
+    },
+    vuefire: {
+        auth: {
+            enabled: true,
+        },
+        config: {
+            apiKey: process.env.apiKey,
+            authDomain: process.env.authDomain,
+            projectId: process.env.projectId,
+            storageBucket: process.env.storageBucket,
+            messagingSenderId: process.env.messagingSenderId,
+            appId: process.env.appId,
+            measurementId: process.env.measurementId,
         },
     },
 });
