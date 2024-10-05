dd7aa3ca1f5411c303dbbf135668d32cfa084f76
tadashi <tahofu89@gmail.com>
2024-10-05 19:43:44 +0900

    メールアドレスプロバイダを削除しました

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -9,10 +9,7 @@ onMounted(() => {
     $ui.start('#firebaseui-auth-container', {
         signInFlow: 'popup',
         signInSuccessUrl: '/',
-        signInOptions: [
-            firebase.auth.EmailAuthProvider.PROVIDER_ID,
-            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        ],
+        signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
     });
 });
 
