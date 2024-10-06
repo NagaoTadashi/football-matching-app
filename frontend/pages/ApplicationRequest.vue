b6f5b7f8bd7d74cd93f80a6243ba8aa309d2ca08
tadashi <tahofu89@gmail.com>
2024-10-06 22:26:15 +0900

    ApplicationRequest.vueを追加しました

---
+++ frontend/pages/ApplicationRequest.vue
@@ -0,0 +1,71 @@
+<script setup>
+import { ref } from 'vue';
+
+const user = await getCurrentUser();
+const idToken = await user.getIdToken();
+
+const { data: recruitments } = await useFetch(
+    'http://localhost:8000/my_team_recruitments',
+    {
+        method: 'GET',
+        headers: {
+            Authorization: `Bearer ${idToken}`,
+            'Content-Type': 'application/json',
+        },
+    }
+);
+
+const dialog = ref(false);
+const dialogDelete = ref(false);
+const headers = ref([
+    { title: '年', align: 'start', key: 'year', sortable: false },
+    { title: '月', key: 'month', sortable: false },
+    { title: '日', key: 'day', sortable: false },
+    { title: '開始時間', key: 'start_time', sortable: false },
+    { title: '終了時間', key: 'end_time', sortable: false },
+    { title: '場所', key: 'location', sortable: false },
+    { title: 'チーム', key: 'name', sortable: false },
+    { title: 'Actions', key: 'actions', sortable: false },
+]);
+</script>
+
+<template>
+    <div>
+        <v-data-table
+            :headers="headers"
+            :items="recruitments"
+            :sort-by="[
+                { key: 'year', order: 'desc' },
+                { key: 'month', order: 'desc' },
+                { key: 'day', order: 'desc' },
+            ]"
+        >
+            <template v-slot:top>
+                <v-toolbar flat>
+                    <v-toolbar-title>他チームからの申込一覧</v-toolbar-title>
+                </v-toolbar>
+            </template>
+            <template v-slot:[`item.actions`]="{ item }">
+                <v-icon
+                    class="me-2"
+                    color="#4CAF50"
+                    @click="editItem(item)"
+                    v-tooltip:top="'承認'"
+                >
+                    mdi-check-circle-outline
+                </v-icon>
+                <v-icon
+                    color="#F44336"
+                    class="me-2"
+                    @click="deleteItem(item)"
+                    v-tooltip:top="'辞退'"
+                >
+                    mdi-close-circle
+                </v-icon>
+            </template>
+            <template v-slot:no-data>
+                他チームからの申し込みはまだありません
+            </template>
+        </v-data-table>
+    </div>
+</template>
