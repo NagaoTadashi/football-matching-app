dcbedf5927356e892f638ae3c537055c01c1ed18
tadashi <tahofu89@gmail.com>
2024-10-06 22:28:45 +0900

    文言を変更しました

--- frontend/pages/RequirementOpponent.vue
+++ frontend/pages/RequirementOpponent.vue
@@ -177,7 +177,7 @@ function required(v) {
         >
             <template v-slot:top>
                 <v-toolbar flat>
-                    <v-toolbar-title>投稿済み募集一覧</v-toolbar-title>
+                    <v-toolbar-title>投稿済みの募集一覧</v-toolbar-title>
                     <v-divider class="mx-4" inset vertical></v-divider>
                     <v-spacer></v-spacer>
 
