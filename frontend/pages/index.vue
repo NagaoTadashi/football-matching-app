5ccfdb19938ecfe158eb7ab9411fb3586260f76f
tadashi <tahofu89@gmail.com>
2024-10-12 19:57:17 +0900

    詳細ボタンを押した際の挙動までを実装しました

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,7 +1,32 @@
 <script setup>
-import MatchCard from '~/components/pages/matchlist/MatchCard.vue';
+const user = await getCurrentUser();
+const idToken = await user.getIdToken();
 
-const { data: matches } = await useFetch('http://localhost:8000/matches');
+const { data: matches } = await useFetch('http://localhost:8000/matches', {
+    method: 'GET',
+    headers: {
+        Authorization: `Bearer ${idToken}`,
+        'Content-Type': 'application/json',
+    },
+});
+
+const { data: myTeam } = await useFetch('http://localhost:8000/team_info', {
+    method: 'GET',
+    headers: {
+        Authorization: `Bearer ${idToken}`,
+        'Content-Type': 'application/json',
+    },
+});
+
+console.log(matches.value);
+
+const search = shallowRef('');
+
+const img_url =
+    // 'https://images.pexels.com/photos/41257/pexels-photo-41257.jpeg?auto=compress&cs=tinysrgb&w=800';
+    // 'https://images.pexels.com/photos/1884574/pexels-photo-1884574.jpeg?auto=compress&cs=tinysrgb&w=800';
+    // 'https://images.pexels.com/photos/18420917/pexels-photo-18420917.jpeg?auto=compress&cs=tinysrgb&w=800';
+    'https://cdn.pixabay.com/photo/2015/07/02/00/08/football-828218_1280.jpg';
 </script>
 
 <template>
@@ -18,11 +43,162 @@ const { data: matches } = await useFetch('http://localhost:8000/matches');
             </v-empty-state>
         </div>
         <div v-else>
-            <v-row>
-                <v-col v-for="(match, i) in matches" :key="i" cols="12" md="4">
-                    <MatchCard :match="match" />
-                </v-col>
-            </v-row>
+            <v-data-iterator
+                :items="matches"
+                :items-per-page="3"
+                :search="search"
+            >
+                <template v-slot:header>
+                    <v-toolbar class="px-2">
+                        <v-text-field
+                            v-model="search"
+                            density="comfortable"
+                            placeholder="Search"
+                            prepend-inner-icon="mdi-magnify"
+                            style="max-width: 300px"
+                            variant="solo"
+                            clearable
+                            hide-details
+                        ></v-text-field>
+                    </v-toolbar>
+                </template>
+
+                <template v-slot:default="{ items }">
+                    <v-container class="pa-2" fluid>
+                        <v-row dense>
+                            <v-col
+                                v-for="item in items"
+                                :key="item.title"
+                                cols="auto"
+                                md="4"
+                            >
+                                <v-card class="pb-3" border flat>
+                                    <v-img :src="img_url"></v-img>
+
+                                    <v-card-item>
+                                        <div
+                                            v-if="
+                                                item.raw.home_team_id !=
+                                                myTeam.id
+                                            "
+                                        >
+                                            <v-card-title>
+                                                vs
+                                                {{ item.raw.home_team_name }}
+                                                (A)
+                                            </v-card-title>
+
+                                            <v-card-subtitle>
+                                                {{
+                                                    item.raw
+                                                        .home_team_prefecture
+                                                }}
+                                                |
+                                                {{
+                                                    item.raw.home_team_category
+                                                }}
+                                                |
+                                                {{ item.raw.home_team_league }}
+                                            </v-card-subtitle>
+                                        </div>
+
+                                        <div v-else>
+                                            <v-card-title>
+                                                vs
+                                                {{
+                                                    item.raw.away_team_name
+                                                }}
+                                                (H)
+                                            </v-card-title>
+
+                                            <v-card-subtitle>
+                                                {{
+                                                    item.raw
+                                                        .away_team_prefecture
+                                                }}
+                                                |
+                                                {{
+                                                    item.raw.away_team_category
+                                                }}
+                                                |
+                                                {{ item.raw.away_team_league }}
+                                            </v-card-subtitle>
+                                        </div>
+
+                                        <v-card-subtitle>
+                                            <v-icon>mdi-calendar-month</v-icon>
+                                            {{ item.raw.year }}年{{
+                                                item.raw.month
+                                            }}月{{ item.raw.day }}日
+                                        </v-card-subtitle>
+                                        <v-card-subtitle>
+                                            <v-icon>
+                                                mdi-clock-time-eight-outline
+                                            </v-icon>
+                                            {{ item.raw.start_time }}
+                                            ~
+                                            {{ item.raw.end_time }}
+                                        </v-card-subtitle>
+
+                                        <v-card-subtitle
+                                            ><v-icon>
+                                                mdi-map-marker-outline</v-icon
+                                            >{{
+                                                item.raw.location
+                                            }}</v-card-subtitle
+                                        >
+                                    </v-card-item>
+
+                                    <div
+                                        class="d-flex justify-space-between px-4"
+                                    >
+                                        <div
+                                            class="d-flex align-center text-caption text-medium-emphasis me-1"
+                                        ></div>
+
+                                        <v-btn
+                                            class="text-none"
+                                            size="small"
+                                            text="詳細"
+                                            border
+                                            flat
+                                        >
+                                        </v-btn>
+                                    </div>
+                                </v-card>
+                            </v-col>
+                        </v-row>
+                    </v-container>
+                </template>
+
+                <template
+                    v-slot:footer="{ page, pageCount, prevPage, nextPage }"
+                >
+                    <div class="d-flex align-center justify-center pa-4">
+                        <v-btn
+                            :disabled="page === 1"
+                            density="comfortable"
+                            icon="mdi-arrow-left"
+                            variant="tonal"
+                            rounded
+                            @click="prevPage"
+                        ></v-btn>
+
+                        <div class="mx-2 text-caption">
+                            Page {{ page }} of {{ pageCount }}
+                        </div>
+
+                        <v-btn
+                            :disabled="page >= pageCount"
+                            density="comfortable"
+                            icon="mdi-arrow-right"
+                            variant="tonal"
+                            rounded
+                            @click="nextPage"
+                        ></v-btn>
+                    </div>
+                </template>
+            </v-data-iterator>
         </div>
     </div>
 </template>
