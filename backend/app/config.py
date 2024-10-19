ed87ce6b2774a8c732195b471ba35559b021e9cb
tadashi <tahofu89@gmail.com>
2024-10-19 11:48:14 +0900

    config.pyを追加しました

---
+++ backend/app/config.py
@@ -0,0 +1,16 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from functools import lru_cache
+import os
+
+
+class Settings(BaseSettings):
+    database_url: str
+
+    model_config = SettingsConfigDict(
+        env_file=os.path.join(os.path.dirname(__file__), "../.env")
+    )
+
+
+@lru_cache
+def get_settings():
+    return Settings()
