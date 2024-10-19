085e96157f0cc609f668b769345f88ccbe331c61
tadashi <tahofu89@gmail.com>
2024-10-19 11:48:32 +0900

    環境変数から読み込むように実装を変更しました

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,8 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from .config import get_settings
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+settings = get_settings()
+
+SQLALCHEMY_DATABASE_URL = settings.database_url
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(
