340a5b7176ef023f54b56fc026ae29af891f7c22
tadashi <tahofu89@gmail.com>
2024-10-20 17:13:51 +0900

    不要な部分を削除しました

--- backend/app/database.py
+++ backend/app/database.py
@@ -8,9 +8,7 @@ settings = get_settings()
 SQLALCHEMY_DATABASE_URL = settings.database_url
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
