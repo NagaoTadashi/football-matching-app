72256094d292344ef513bd0a316b041f14cffd16
tadashi <tahofu89@gmail.com>
2024-10-20 17:13:18 +0900

    環境によって.envファイルを切り替えるように変更しました

--- backend/app/config.py
+++ backend/app/config.py
@@ -7,10 +7,10 @@ class Settings(BaseSettings):
     database_url: str
 
     model_config = SettingsConfigDict(
-        env_file=os.path.join(os.path.dirname(__file__), "../.env")
+        env_file=os.path.join(os.path.dirname(__file__), f"../.env.{os.environ.get('ENV', 'development')}")
     )
 
-
+    
 @lru_cache
 def get_settings():
     return Settings()
